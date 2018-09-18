b6da95bf11ee547282061dcc7f7f42c4c35fc304
Vinicius Batista de Oliveira <vinicius.batistaoli@gmail.com>
2018-09-18 11:36:35 -0300

    Criacao models Cidade e Estado

--- src/main/java/com/vinicius/demo/DemoApplication.java
+++ src/main/java/com/vinicius/demo/DemoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vinicius.demo.domain.Categoria;
+import com.vinicius.demo.domain.Cidade;
+import com.vinicius.demo.domain.Estado;
 import com.vinicius.demo.domain.Produto;
 import com.vinicius.demo.repositories.CategoriaRepository;
+import com.vinicius.demo.repositories.CidadeRepository;
+import com.vinicius.demo.repositories.EstadoRepository;
 import com.vinicius.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,7 +24,13 @@ public class DemoApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -42,8 +52,22 @@ public class DemoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+	
 	}
 }
--- src/main/java/com/vinicius/demo/domain/Estado.java
+++ src/main/java/com/vinicius/demo/domain/Estado.java
@@ -8,14 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Estado implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -23,19 +19,19 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
-	private String nome;
+	private String Nome;
 	
-	@JsonManagedReference
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
 	
-	public Categoria() {
+	public Estado() {
+		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
-		this.nome = nome;
+		Nome = nome;
 	}
 
 	public Integer getId() {
@@ -47,19 +43,19 @@ public class Categoria implements Serializable {
 	}
 
 	public String getNome() {
-		return nome;
+		return Nome;
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
+		Nome = nome;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public List<Cidade> getCidades() {
+		return cidades;
+	}
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
 
 	@Override
@@ -78,7 +74,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -86,5 +82,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-
+	
+	
+	
+	
 }
