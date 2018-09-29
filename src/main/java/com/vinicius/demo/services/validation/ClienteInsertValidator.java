fc35795ca5bde0112cb8e38a18f3c52f17b938da
Vinicius Batista de Oliveira <vinicius.batistaoli@gmail.com>
2018-09-29 11:54:25 -0300

    Anotação personalizada, validação cpf e cnpj

---
+++ src/main/java/com/vinicius/demo/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,43 @@
+package com.vinicius.demo.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.vinicius.demo.domain.enums.TipoCliente;
+import com.vinicius.demo.dto.ClienteNewDTO;
+import com.vinicius.demo.resources.exception.FieldMessage;
+import com.vinicius.demo.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) 
+				&& !BR.isValidCPF(objDto.getCpfOuCnpj())  ) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) 
+				&& !BR.isValidCNPJ(objDto.getCpfOuCnpj())  ) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		//Percorrer a lista de field message e adiciona na lista de erro do framework
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		} // vai lançar o erro no meu exception handler
+		
+		return list.isEmpty();
+	}
+}
