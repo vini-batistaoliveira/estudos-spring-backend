376d7808f24e528c0cf9d4f4ed1aa273efec2f49
Vinicius Batista de Oliveira <vinicius.batistaoli@gmail.com>
2018-10-01 17:56:08 -0300

    Inserindo Pedido

--- src/main/java/com/vinicius/demo/resources/PedidoResource.java
+++ src/main/java/com/vinicius/demo/resources/PedidoResource.java
@@ -1,16 +1,23 @@
 package com.vinicius.demo.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vinicius.demo.domain.Categoria;
 import com.vinicius.demo.domain.Pedido;
+import com.vinicius.demo.dto.CategoriaDTO;
 import com.vinicius.demo.services.PedidoService;
 
 @RestController
@@ -27,5 +34,15 @@ public class PedidoResource {
 
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
 	
 }
--- src/main/java/com/vinicius/demo/services/PedidoService.java
+++ src/main/java/com/vinicius/demo/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.vinicius.demo.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vinicius.demo.domain.ItemPedido;
+import com.vinicius.demo.domain.PagamentoComBoleto;
 import com.vinicius.demo.domain.Pedido;
+import com.vinicius.demo.domain.enums.EstadoPagamento;
+import com.vinicius.demo.repositories.ItemPedidoRepository;
+import com.vinicius.demo.repositories.PagamentoRepository;
 import com.vinicius.demo.repositories.PedidoRepository;
 import com.vinicius.demo.services.exceptions.ObjectNotFoundException;
 
@@ -14,14 +20,51 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public Pedido find(Integer id) {
-		
+
 		Optional<Pedido> obj = repo.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
 		
+		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
+		
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		
+		for(ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
+
 
 }
